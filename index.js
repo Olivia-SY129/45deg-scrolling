ba7d6bcd97e56d01aebf20322fe0dfebadf59a61
Seungyeon Lee <sie9412@gmail.com>
2022-04-19 16:09:13 +0900

    Fix: x-axis movement bug fixed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const wrap = document.querySelector('.wrap');
-wrap.style.transform = "rotateX(45deg) rotateZ(30deg)";
+const wrap = document.querySelector(".wrap");
 
 function scrollEventHandler() {
-    const px = window.pageYOffset/90;
-    wrap.style.transform = `translateX(${-200+73.5*px}px) translateY(-200px) rotateX(45deg) rotateZ(30deg)`;
+  const offset = window.pageYOffset / 90;
+  wrap.style.transform = `translateX(${
+    -200 + 73.5 * offset
+  }px) translateY(-200px) rotateX(45deg) rotateZ(30deg)`;
 }
 
-document.addEventListener('scroll', scrollEventHandler);
+document.addEventListener("scroll", scrollEventHandler);
